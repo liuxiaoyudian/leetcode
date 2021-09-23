6b7e94c8e58f48b07ca0c0a1b6f30f71ce781b18
liuxiaoyudian <liuxiaoyudian@outlook.com>
2021-09-23 23:16:12 +0100

    2021-09-23

--- src/剑指offer/二分查找/Solution11.java
+++ src/剑指offer/二分查找/Solution11.java
@@ -6,18 +6,19 @@ package 剑指offer.二分查找;
  */
 public class Solution11 {
 
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 0, 1};
+        System.out.println(minNumberInRotateArray1(nums));
+    }
+
     /**
      * 暴力法
      * 直接遍历一遍数组，即可找到最小值。但是本题的附加条件就没有用上。肯定不是面试官所期望的答案。
      */
-    public int minNumberInRotateArray(int [] array) {
-        int n = array.length;
-        if (n == 0) {
-            return 0;
-        }
-        int min = array[0];
-        for (int i = 1; i < n; i++) {
-            min = Math.min(min, array[i]);
+    public int minNumberInRotateArray(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int i : array) {
+            min = Math.min(min, i);
         }
         return min;
     }
@@ -26,19 +27,15 @@ public class Solution11 {
     /**
      * 利用二分查找的思想
      */
-    public int minNumberInRotateArray1(int [] array) {
-        int n = array.length;
-        if (n == 0) {
-            return 0;
-        }
-        int l = 0, r = n - 1;
+    public static int minNumberInRotateArray1(int[] array) {
+        int l = 0, r = array.length - 1;
         while (l <= r) { // l < r也可以
             int m = l + ((r - l) >> 1);
-            if (array[m] == array[r]) {
+            if (array[m] == array[r]) { // 出现这种情况的array类似 [1,0,1,1,1] 或者[1,1,1,0,1]，此时最小数字不好判断在mid左边还是右边,这时只好一个一个试
                 r--;
-            } else if (array[m] > array[r]) {
+            } else if (array[m] > array[r]) { // 最小值在右边（不包含m），出现这种情况的array类似[3,4,5,6,0,1,2]，此时最小数字一定在mid的右边
                 l = m + 1;
-            } else if (array[m] < array[r]) {
+            } else if (array[m] < array[r]) { // 最小值在左边（可能包含m），出现这种情况的array类似[2,2,3,4,5,6,6],此时最小数字一定就是array[mid]或者在mid的左边。因为右边必然都是递增的。
                 r = m;
             }
         }
--- src/剑指offer/二分查找/Solution53.java
+++ src/剑指offer/二分查找/Solution53.java
@@ -1,37 +1,34 @@
 package 剑指offer.二分查找;
 
+/**
+ * 53. 数字在排序数组中出现的次数
+ * https://github.com/CyC2018/CS-Notes/blob/master/notes/53.%20数字在排序数组中出现的次数.md
+ */
 public class Solution53 {
 
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 3, 3, 3, 4, 5};
-        System.out.println(GetNumberOfK(arr, 3));
-    }
-
-
-    public static int GetNumberOfK(int [] array , int k) {
-        // 寻找左侧边界的二分搜索
-        int i = 0;
-        int j = array.length;
-        while (i < j) {
-            int mid = i + (j - i) / 2;
-            if (k == array[mid]) {
-                j = mid; // 关键点在这里，找到了，并没有立马返回，而是一直搜小范围
-            } else if (k < array[mid]) {
-                j = mid;
-            } else if (k > array[mid]) {
-                i = mid + 1;
+    /**
+     * 寻找左侧边界的二分搜索
+     */
+    public int GetNumberOfK(int [] array , int k) {
+        int l = 0, r = array.length;
+        while (l < r) {
+            int m = l + ((r - l) >> 1);
+            if (k == array[m]) {
+                r = m; // // 关键点在这里，找到后，并没有立马返回，而是一直搜小范围
+            } else if (k < array[m]) {
+                r = m;
+            } else if (k > array[m]) {
+                l = m + 1;
             }
         }
-//         if (i == array.length || array[i] != k) {
-//             return 0;
-//         }
-        int count = 0; // 对于没有出现过的数字来说，就是0
-        for (;i < array.length; i++) {
-            if (array[i] == k) {
-                count++;
+        int cnt = 0;
+        for (int i = l; i < array.length; i++) {
+            if (array[i] != k) {
+                break;
             }
+            cnt++;
         }
-        return count;
+        return cnt;
     }
 
 }
